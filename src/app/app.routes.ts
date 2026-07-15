2b358b33b6d3a8a49f606be82bd918bbadaceb30
Gabriela1011 <lionte.gabriela@yahoo.com>
2026-07-15 17:08:50 +0300

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Homepage } from './homepage/homepage';
+import { Search } from './search/search';
+import { Tasks } from './tasks/tasks';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: Homepage},
+    {path: 'search', component: Search},
+    {path: 'myTasks', component: Tasks},
+    // {path: '', redirectTo: '/home', pathMatch: 'full'},
+    // {path: '**', component: NotFoundComponent}
+];
---
+++ src/app/tasks/tasks.ts
@@ -0,0 +1,20 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+
+@Component({
+  selector: 'app-tasks',
+  imports: [],
+  templateUrl: './tasks.html',
+  styleUrl: './tasks.css',
+})
+export class Tasks implements OnInit {
+  private taskService = inject(TaskService);
+  tasks = signal<any[]>([]);
+ 
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe(res => {
+      this.tasks.set(res);
+      console.log('Tasks fetched:', this.tasks());
+    });
+  }
+}
